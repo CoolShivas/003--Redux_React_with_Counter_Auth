de6c6fbeaf9e2398811428ba7f99af7c6112f988
CoolShivas <shivas2710cool00@gmail.com>
2024-05-23 17:18:11 +0530

    Using useSelector to take out the slice from the reducer

--- Redux_React_with_Counter_Auth/src/DisplayValue.jsx
+++ Redux_React_with_Counter_Auth/src/DisplayValue.jsx
@@ -2,11 +2,13 @@ import { useSelector } from "react-redux";
 
 const DisplayValue = () => {
 
-    const value = useSelector(store => store.count);
+    // const value = useSelector(store => store.count);
+    const { counterVal } = useSelector((store) => store.counter);
 
     return (
         <p className="bg-white fs-5 mb-4">
-            Counter Value is = {value}.
+            {/* Counter Value is = {value}. */}
+            Counter Value is = {counterVal}.
         </p>
     )
 }
